294bda4bdf555ebba08dd8f194115001897ad8ba
Aryan Deora <adeora@iu.edu>
2023-01-03 20:13:54 -0500

    Query Devtools Init

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import {
   createSignal,
   Show,
   Suspense,
+  ErrorBoundary,
 } from "solid-js";
 import styles from "./App.module.css";
 
 import {
   createQuery,
   defaultContext,
+  QueryFunction,
 } from "../../query/packages/solid-query/src";
+
+import { SolidQueryDevtools } from "./devtools/SolidQueryDevtools";
 
 interface Post {
   userId: number;
@@ -24,46 +28,56 @@ const App: Component = () => {
 
   return (
     <div class={styles.App}>
+      <SolidQueryDevtools />
       <p>Post ID: {posts()}</p>
 
       <button onClick={() => setPosts(posts() + 1)}>Increment</button>
       <button onClick={() => setPosts(posts() - 1)}>Decrement</button>
-      <Suspense fallback={<div>Suspended...</div>}>
-        <Wrapper posts={posts()} />
-      </Suspense>
+      <h1>Posts</h1>
+      <ErrorBoundary
+        fallback={(_err, resetSolid) => (
+          <div>
+            <div>error boundary</div>
+            <button
+              onClick={() => {
+                succeed = true;
+                resetSolid();
+              }}
+            >
+              retry
+            </button>
+          </div>
+        )}
+      >
+        <Suspense fallback={"Loading..."}>
+          <Wrapper />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 };
 
-const Wrapper: Component<{ posts: number }> = (props) => {
-  const postsQuery = createQuery(() => ({
-    queryKey: ["posts", props.posts],
-    queryFn: async ({ queryKey }) => {
-      const [key, id] = queryKey;
-      // await new Promise((resolve) => setTimeout(resolve, 3000));
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts/" + id
-      );
-      const data = await response.json();
-      return data as Post;
+let succeed = false;
+const Wrapper: Component = () => {
+  const state = createQuery(() => ({
+    queryKey: ["wrapper"],
+    queryFn: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      if (!succeed) {
+        throw new Error("Suspense Error Bingo");
+      } else {
+        return "data";
+      }
     },
-    select(data) {
-      return data.title;
-    },
-    enabled: props.posts > 0,
+    retryDelay: 10,
+    throwErrors: true,
   }));
 
-  createEffect(() => {
-    console.log("postsQuery", postsQuery.data);
-  });
-
+  // Suspense only triggers if used in JSX
   return (
-    <div>
-      {postsQuery.isLoading && <div>Loading...</div>}
-      <Show when={postsQuery.data} keyed>
-        {(title) => <div>{title}</div>}
-      </Show>
-    </div>
+    <Show when={state.data}>
+      <div>rendered</div>
+    </Show>
   );
 };
 export default App;
---
+++ src/devtools/SolidQueryDevtools.tsx
@@ -0,0 +1,10 @@
+import "./index.css";
+
+export function SolidQueryDevtools() {
+  return (
+    <aside id="SolidQueryDevtoolsPanel">
+      <h1 class="sqd-text-green-500">Hello world!</h1>
+      <button>Hello</button>
+    </aside>
+  );
+}
